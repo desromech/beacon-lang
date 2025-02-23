5f71c5bdf5a602dd6a36fa000a48748ae95282b3
Ronie Salgado <roniesalg@gmail.com>
2025-02-23 14:19:06 -0300

    Start using SDL2.

---
+++ src/beacon-vm/SDL2Window.c
@@ -0,0 +1,27 @@
+#include "Context.h"
+#include "Window.h"
+
+static bool hasInitializedSDL2;
+
+static beacon_oop_t beacon_Window_open(beacon_context_t *context, beacon_oop_t receiver, size_t argumentCount, beacon_oop_t *arguments)
+{
+    (void)context;
+    (void)receiver;
+    (void)argumentCount;
+    (void)arguments;
+    return receiver;
+}
+static beacon_oop_t beacon_WindowClass_enterMainLoop(beacon_context_t *context, beacon_oop_t receiver, size_t argumentCount, beacon_oop_t *arguments)
+{
+    (void)context;
+    (void)receiver;
+    (void)argumentCount;
+    (void)arguments;
+    return receiver;
+}
+
+void beacon_context_registerWindowSystemPrimitives(beacon_context_t *context)
+{
+    beacon_addPrimitiveToClass(context, context->classes.windowClass, "open", 0, beacon_Window_open);
+    beacon_addPrimitiveToClass(context, beacon_getClass(context, (beacon_oop_t)context->classes.windowClass), "enterMainLoop", 0, beacon_WindowClass_enterMainLoop);
+}
